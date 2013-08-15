96c86122a260f3087d36f66db0b47a6a174d1030
Aditya <asdocode@gmail.com>
2013-08-15 18:58:15 +0530

    Update readdb.py

--- readdb.py
+++ readdb.py
@@ -4,33 +4,34 @@ from PyQt4.QtCore import pyqtSlot,SIGNAL,SLOT
 from PyQt4 import QtSql
 
 
-
 def main():
-        app = QtGui.QApplication(sys.argv)
-        db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
-        db.setDatabaseName("SwiftTyper")
-        ok = db.open()
-        if not ok:
-            msg = QtGui.QMessageBox()
-            msg.setText("Cannot connect to the database")
-            msg.exec_()
-            sys.exit()
+	app = QtGui.QApplication(sys.argv)
+	db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+	
+	db.setDatabaseName("SwiftTyper")
+	
+	
+
+	ok = db.open()
+	if not ok:
+		msg = QtGui.QMessageBox()
+		msg.setText("Cannot connect to the database..!")
+		msg.exec_()
+		sys.exit()
 
 	query = QtSql.QSqlQuery()
-        query.exec_("SELECT name,credenzID,college FROM reg_table")
-			
-	result = "Name\tCredenz ID\t\tcollege"
-	while query.next():
-		name = query.value(0).toString()
-		email = query.value(1).toString()
-		wpm = query.value(2).toString()
-		accuracy = query.value(3).toString()
-		result += ("\n" + name + "\t" + email + "\t" + wpm + "\t" + accuracy)
-	msg = QtGui.QMessageBox()
-	msg.setText(result)
-	
-	msg.exec_()
+	ok = query.exec_("CREATE TABLE reg_table(name varchar(20), credenzID varchar(20), college varchar(25),phno int)")
+	if ok:
+		msg = QtGui.QMessageBox()
+		msg.setText("You are now registered.")
+		msg.exec_()
+	else:
+		msg = QtGui.QMessageBox()
+		msg.setText("Error creating table")
+		msg.exec_()
 
+	sys.exit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
